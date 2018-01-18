f1a816097f0e68f338b7e72c6a3a2abf268d0b7e
DercilioFontes <dercilioafontes@gmail.com>
2018-01-18 23:38:42 +0000

    Added anonymous function to sort numbers.

---
+++ sort-numbers.js
@@ -0,0 +1,13 @@
+var array = [10, 2, 5, 1, 9];
+
+array.sort(function(first, second) {
+  return first - second;
+});
+
+console.log(array);
+
+
+
+
+
+// Read the above article and write JavaScript code to sort the array numerically (the sorted array should equal [1, 2, 5, 9, 10])
