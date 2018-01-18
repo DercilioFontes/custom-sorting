923d80d6085c3153c2c7b2714d32eb6ffa391847
DercilioFontes <dercilioafontes@gmail.com>
2018-01-18 23:28:11 +0000

    First solution for Advanced Sorting (by name first, by age if names are equal).

---
+++ custom-sorting.js
@@ -0,0 +1,40 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+// var customComparator = function sortbyNamebyAge(first, second) {
+//   console.log(first.name);
+//   console.log(second.name);
+//   if (first.name < second.name) {
+//     return -1;
+//   } else {
+//     return first.age - second.age;
+//   }
+// }
+
+students.sort(function sortbyNamebyAge(first, second) {
+  console.log(first.name);
+  console.log(second.name);
+  if (first.name < second.name) {
+    return -1;
+  } else if (first.name === second.name) {
+    return first.age - second.age;
+  } else {
+    return 1;
+  }
+});
+
+console.log(students);
+
+//Write a sorting function (sometimes called a custom comparator), that sorts the above array first by the name ascending alphabetically, and in cases where the names are equal sort by descending age.
+
+
+// http://www.javascriptkit.com/javatutors/arraysort.shtml
+// When such a function is passed into array.sort(), the array elements are sorted based on the relationship between each pair of elements "a" and "b" and the function's return value. The three possible return numbers are: <0 (less than 0), 0, or >0 (greater than 0):
+
+// Less than 0: Sort "a" to be a lower index than "b"
+// Zero: "a" and "b" should be considered equal, and no sorting performed.
+// Greater than 0: Sort "b" to be a lower index than "a".
